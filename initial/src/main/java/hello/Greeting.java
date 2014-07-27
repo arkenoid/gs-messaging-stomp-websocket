a373ad68edd8882474419d313f55c712cf796c56
Anantya Wibowo <anantyaw@gmail.com>
2014-07-27 11:54:12 +0700

    nice sample.. ntar aku implemen ya

---
+++ initial/src/main/java/hello/Greeting.java
@@ -0,0 +1,18 @@
+package hello;
+
+/**
+ * Created by arke on 27/07/14.
+ */
+public class Greeting {
+
+    private String content;
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
---
+++ initial/src/main/java/hello/HelloMessage.java
@@ -0,0 +1,13 @@
+package hello;
+
+/**
+ * Created by arke on 27/07/14.
+ */
+public class HelloMessage {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+}
